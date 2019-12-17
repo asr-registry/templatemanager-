0e95dcde53df13260903793b465dbf53c2e8b5df
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-17 10:16:03 +0430

    exceptions

---
+++ src/main/java/af/asr/templatemanager/exception/AuthZException.java
@@ -0,0 +1,37 @@
+package af.asr.templatemanager.exception;
+
+
+import af.asr.authadapter.exception.common.BaseUncheckedException;
+import af.asr.authadapter.exception.common.ServiceError;
+
+import java.util.List;
+
+public class AuthZException extends BaseUncheckedException {
+
+	/**
+	 * Serializable version ID.
+	 */
+	private static final long serialVersionUID = 8152409863253682472L;
+
+	/**
+	 * This variable holds the MosipErrors list.
+	 */
+	private final List<ServiceError> list;
+
+	/**
+	 * @param list
+	 *            The error list.
+	 */
+	public AuthZException(List<ServiceError> list) {
+		this.list = list;
+	}
+
+	/**
+	 * Getter for error list.
+	 * 
+	 * @return The error list.
+	 */
+	public List<ServiceError> getList() {
+		return list;
+	}
+}
